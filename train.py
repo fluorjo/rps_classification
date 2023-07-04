bbfd431843573d1bb04f08d035faeb33795847d0
fluorjo <fluorjo@naver.com>
2023-07-04 12:52:14 +0900

    device, loader

--- train.py
+++ train.py
@@ -11,3 +11,11 @@ hidden_size=500
 num_classes=3
 lr=0.001
 epochs=3
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+train_dataset=''
+test_dataset=''
+
+train_loader=DataLoader(dataset=train_dataset,batch_size=batch_size,shuffle=True)
+test_loader=DataLoader(dataset=test_dataset,batch_size=batch_size,shuffle=True)
