c93647aa9386016e74ed09af5bd6657b7391715a
fluorjo <fluorjo@naver.com>
2023-07-06 12:29:07 +0900

    mlp

--- train.py
+++ train.py
@@ -19,3 +19,41 @@ test_dataset=''
 
 train_loader=DataLoader(dataset=train_dataset,batch_size=batch_size,shuffle=True)
 test_loader=DataLoader(dataset=test_dataset,batch_size=batch_size,shuffle=True)
+
+class myMLP(nn.Module):
+    def __init__(self, hidden_size, num_classes):
+        super().__init__()
+        self.fc1=nn.Linear(28*28,hidden_size)
+        self.fc2=nn.Linear(hidden_size,hidden_size)
+        self.fc3=nn.Linear(hidden_size,hidden_size)
+        self.fc4=nn.Linear(hidden_size,hidden_size)
+    def forward(self, x):
+        b,w,h,c = x.shape
+        x=x.reshape(-1,28*28)
+        
+        x=self.fc1(x)
+        x=self.fc2(x)
+        x=self.fc3(x)
+        x=self.fc4(x)
+        
+        return x
+
+model=myMLP(hidden_size,num_classes).to(device)
+loss=nn.CrossEntropyLoss()
+optim=Adam(model.parameters(),lr=lr)
+
+for epoch in range(epochs):
+    for idx, (image, target) in enumerate(train_loader):
+        image=image.to(device)
+        target=target.to(device)
+        
+        out=model(image)
+        loss_value=loss(out,target)
+        
+        optim.zero_grad()
+        
+        loss_value.backward()
+        optim.step()
+        
+        if idx%100 ==0:
+            print(loss_value.item())
