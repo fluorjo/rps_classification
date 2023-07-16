a6c5376073f8e56884dd7b7c5864017b75de730c
fluorjo <fluorjo@naver.com>
2023-07-16 18:58:11 +0900

    참고 깃허브

--- ref-teddynote.py
+++ ref-teddynote.py
@@ -2,3 +2,16 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset
 from torchvision import datasets, transforms
+
+# 이미지 데이터셋 다운로드
+import urllib.request
+import zipfile
+
+# 데이터셋을 다운로드 합니다.
+# 다운로드 후 tmp 폴더에 압축을 해제 합니다.
+url = 'https://storage.googleapis.com/download.tensorflow.org/data/rps.zip'
+urllib.request.urlretrieve(url, 'rps.zip')
+local_zip = 'rps.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('tmp/')
+zip_ref.close()
